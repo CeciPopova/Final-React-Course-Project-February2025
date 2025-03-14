46f1ad3b5545d0b2ddff3aaebd7c77fad0cb9d88
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2025-03-14 13:45:09 +0200

    add social media links to the footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,3 +1,5 @@
+
+
 export default function Footer() {
     return (
         <div className="footer_section layout_padding">
@@ -6,29 +8,25 @@ export default function Footer() {
                     <div className="col-md-12">
                         <div className="footer_social_icon">
                             <ul>
-                                <li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
+                                <li><a href="https://www.facebook.com/ceci.popova"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                <li><a href="https://x.com/PopovaCeci"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                <li><a href="https://www.linkedin.com/in/ceci-popova-2895a7219/"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                <li><a href="https://www.instagram.com/cecipopova/"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
                             </ul>
                         </div>
                         <div className="location_text">
                             <ul>
                                 <li>
-                                    <a href="#">
-                                        <i className="fa fa-phone" aria-hidden="true"></i><span className="padding_left_10">+01 1234567890</span>
+                                    <a href="https://globfone.com/call-phone/">
+                                        <i className="fa fa-phone" aria-hidden="true"></i><span className="padding_left_10">+30 6984203699</span>
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="#">
-                                        <i className="fa fa-envelope" aria-hidden="true"></i><span className="padding_left_10">demo@gmail.com</span>
+                                    <a href="https://account.microsoft.com/profile">
+                                        <i className="fa fa-envelope" aria-hidden="true"></i><span className="padding_left_10">popova_cip@hotmail.com</span>
                                     </a>
                                 </li>
                             </ul>
-                        </div>
-                        <div className="form-group">
-                            <textarea className="update_mail" placeholder="Your Email" rows="5" id="comment" name="Your Email"></textarea>
-                            <div className="subscribe_bt"><a href="#"><i className="fa fa-arrow-right" aria-hidden="true"></i></a></div>
                         </div>
                     </div>
                 </div>
