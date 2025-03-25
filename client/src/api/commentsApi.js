6e60ee98e86c8fd0a0c160f2226a911da2b3eef1
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2025-03-25 15:40:32 +0200

    update commentsApi

--- client/src/api/commentsApi.js
+++ client/src/api/commentsApi.js
@@ -6,23 +6,23 @@ const baseUrl = 'http://localhost:3030/data/comments';
 
 function commentsReducer(state, action) {
     switch (action.type) {
-        case 'ADD_COMMENT': 
+        case 'ADD_COMMENT':
             return [...state, action.payload]
         case 'GET_ALL':
-           return action.payload; 
+            return action.payload;
         default:
             return state;
     }
 };
 
-export const useComments = (gameId) => {
+export const useComments = (coffeeId) => {
     const { accessToken } = useAuth();
     // const [comments, setComments] = useState([]);
     const [comments, dispatch] = useReducer(commentsReducer, [])
 
     useEffect(() => {
         const searchParams = new URLSearchParams({
-            where: `gameId="${gameId}"`,
+            where: `coffeeId="${coffeeId}"`,
             load: `author=_ownerId:users`,
         });
 
@@ -33,21 +33,21 @@ export const useComments = (gameId) => {
         }
 
         request.get(`${baseUrl}?${searchParams.toString()}`, null, options)
-            .then(result => dispatch({type: 'GET_ALL', payload: result}))
-    }, [gameId, accessToken]); // TODO Fix this
+            .then(result => dispatch({ type: 'GET_ALL', payload: result }))
+    }, [coffeeId, accessToken]); // TODO Fix this
 
     return {
         comments,
-        addComment: (commentData) => dispatch({type: 'ADD_COMMENT', payload: commentData})
+        addComment: (commentData) => dispatch({ type: 'ADD_COMMENT', payload: commentData })
     }
 }
 
 export const useCreateComment = () => {
     const { request } = useAuth();
 
-    const create = (gameId, comment) => {
+    const create = (coffeeId, comment) => {
         const commentData = {
-            gameId,
+            coffeeId,
             comment,
         };
 
