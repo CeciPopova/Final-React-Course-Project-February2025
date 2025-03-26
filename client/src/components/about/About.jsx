d485ec0753c35a9a034d87336bc9357a08815b90
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2025-03-27 00:03:46 +0200

    update about page

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -33,89 +33,14 @@ export default function About() {
                 <p className=" about_text">Our mission is simple: to provide the highest quality coffee while promoting sustainable farming practices that respect both the environment and the communities we work with. We strive to create a meaningful connection between our customers and the source of their coffee, sharing the stories behind each cup.</p>
               </div>
             </div>
-          </div>
-
-
-          
-            <div className="container">
-                <div id="custom_slider" className="carousel slide" data-ride="carousel">
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <div className="row">
-                                <div className="col-md-12">
-                                    <h1 className="about_taital">What syas customers</h1>
-                                </div>
-                            </div>
-                            <div className="client_section_2">
-                                <div className="client_taital_main">
-                                    <div className="client_left">
-                                        <div className="client_img"><img src="images/client-img1.png" /></div>
-                                    </div>
-                                    <div className="client_right">
-                                        <h3 className="moark_text">Joy Moark</h3>
-                                        <p className="client_text">now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancynow use Lorem Ipsum as their default model text, </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div className="row">
-                                <div className="col-md-12">
-                                    <h1 className="about_taital">What syas customers</h1>
-                                </div>
-                            </div>
-                            <div className="client_section_2">
-                                <div className="client_taital_main">
-                                    <div className="client_left">
-                                        <div className="client_img"><img src="images/client-img1.png" /></div>
-                                    </div>
-                                    <div className="client_right">
-                                        <h3 className="moark_text">Joy Moark</h3>
-                                        <p className="client_text">now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancynow use Lorem Ipsum as their default model text, </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div className="row">
-                                <div className="col-md-12">
-                                    <h1 className="about_taital">What syas customers</h1>
-                                </div>
-                            </div>
-                            <div className="client_section_2">
-                                <div className="client_taital_main">
-                                    <div className="client_left">
-                                        <div className="client_img"><img src="images/client-img1.png" /></div>
-                                    </div>
-                                    <div className="client_right">
-                                        <h3 className="moark_text">Joy Moark</h3>
-                                        <p className="client_text">now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancynow use Lorem Ipsum as their default model text, </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <a className="carousel-control-prev" href="#custom_slider" role="button" data-slide="prev">
-                        <i className="fa fa-arrow-left"></i>
-                    </a>
-                    <a className="carousel-control-next" href="#custom_slider" role="button" data-slide="next">
-                        <i className="fa fa-arrow-right"></i>
-                    </a>
-                </div>
-            </div>
+          </div>        
         </div>
         <div className={styles["cheers"]}>
             <p><span className='strong'>Thank you for being a part of our journey!</span></p>
             <p>Cheers,<br />
               The <span className={styles["strong"]}>Coffee Lovers</span> Team</p>
-            {/* <div className="readmore_btn"><a href="#">Read More</a></div> */}
-
           </div>
-
         </div>
-
       </div>
-    
-
   )
 }
