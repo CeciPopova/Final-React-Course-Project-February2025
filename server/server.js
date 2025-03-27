3a8e34df93814262e109ef943e24077730030e34
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2025-03-27 22:14:29 +0200

    update server

--- server/server.js
+++ server/server.js
@@ -1561,7 +1561,7 @@
 
     const server = http__default['default'].createServer(requestHandler(plugins, services));
 
-    const port = 3030;
+    const port = process.env.PORT || 3030;
 
     server.listen(port);
 
