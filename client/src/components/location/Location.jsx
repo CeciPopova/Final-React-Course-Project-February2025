aabdd3b5b653371fa6d57800f0ab705ad4af6d23
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2025-03-29 14:06:19 +0200

    update location

--- client/src/components/location/Location.jsx
+++ client/src/components/location/Location.jsx
@@ -12,7 +12,7 @@ const center = {
     lng: 23.3242,  // Longitude for Sofia, Bulgaria
 };
 
-const GoogleMapComponent = () => {
+export default function GoogleMapComponent() {
   return (
     <LoadScript googleMapsApiKey="AIzaSyDpxXSBzQ683Kr0Yt6rPewOz2XiJYEDjGg">
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
@@ -22,12 +22,3 @@ const GoogleMapComponent = () => {
   );
 };
 
-export default GoogleMapComponent;
-
-    // return (
-    //     <div className="map_main">
-    //         <div className="map-responsive">
-    //             <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&amp;q=Eiffel+Tower+Paris+France" width="250" height="500" frameBorder="0" style={{ "border": "0", "width": "100%" }} allow="fullscreen"></iframe>
-    //         </div>
-    //     </div>
-    // )
