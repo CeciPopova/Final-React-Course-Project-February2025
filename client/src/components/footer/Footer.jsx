f7e8b46a856a9dcf50b84bee1f402df130a2b347
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2025-03-29 14:06:42 +0200

    update footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,45 +4,45 @@ import { Link } from "react-router-dom";
 export default function Footer() {
     return (
         <>
-        <div className="footer_section layout_padding">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="footer_social_icon">
-                            <ul>
-                                <li><a href="https://www.facebook.com/ceci.popova"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                <li><a href="https://x.com/PopovaCeci"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                <li><a href="https://www.linkedin.com/in/ceci-popova-2895a7219/"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                                <li><a href="https://www.instagram.com/cecipopova/"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
-                            </ul>
-                        </div>
-                        <div className="location_text">
-                            <ul>
-                                <li>
-                                    <a href="https://globfone.com/call-phone/">
-                                        <i className="fa fa-phone" aria-hidden="true"></i><span className="padding_left_10">+00 123 456 789</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="https://account.microsoft.com/profile">
-                                        <i className="fa fa-envelope" aria-hidden="true"></i><span className="padding_left_10">coffee_lovers@hotmail.com</span>
-                                    </a>
-                                </li>
-                            </ul>
+            <div className="footer_section layout_padding">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="footer_social_icon">
+                                <ul>
+                                    <li><a href="https://www.facebook.com/ceci.popova"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                    <li><a href="https://x.com/PopovaCeci"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                    <li><a href="https://www.linkedin.com/in/ceci-popova-2895a7219/"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                    <li><a href="https://www.instagram.com/cecipopova/"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
+                                </ul>
+                            </div>
+                            <div className="location_text">
+                                <ul>
+                                    <li>
+                                        <a href="https://globfone.com/call-phone/">
+                                            <i className="fa fa-phone" aria-hidden="true"></i><span className="padding_left_10">+00 123 456 789</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://account.microsoft.com/profile">
+                                            <i className="fa fa-envelope" aria-hidden="true"></i><span className="padding_left_10">coffee_lovers@hotmail.com</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-         <div className="copyright_section">
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-12">
-                        <p className="copyright_text"><Link to="https://github.com/CeciPopova/Final-React-Course-Project-February2025"> Coffee Lovers </Link> 2025 Final Course Project -<Link to="https://github.com/CeciPopova" > Ceci Popova GitHub </Link></p>
+            <div className="copyright_section">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <p className="copyright_text"><Link to="https://github.com/CeciPopova/Final-React-Course-Project-February2025"> Coffee Lovers </Link> 2025 Final Course Project -<Link to="https://github.com/CeciPopova" > Ceci Popova GitHub </Link></p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div> 
 
         </>
     )
